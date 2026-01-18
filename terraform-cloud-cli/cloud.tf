81809a6c5e035b40dc76f29b6944149fc18a2105
surajsanadi <surajsanadi008@gmail.com>
2026-01-18 05:32:36 +0000

    pushing terraform-cloud-cli folder to github

---
+++ terraform-cloud-cli/cloud.tf
@@ -0,0 +1,10 @@
+# terraform { 
+#   cloud { 
+    
+#     organization = "Batch-12surajs" 
+ 
+#     workspaces { 
+#       name = "rg-res" 
+#     } 
+#   } 
+# }
---
+++ terraform-cloud-cli/main.tf
@@ -0,0 +1,43 @@
+#BASIC SYNTAX
+
+# Provider
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.6.1"
+    }
+  }
+
+  cloud { 
+    
+    organization = "Batch-12surajs" 
+ 
+    workspaces { 
+      name = "rg-res" 
+    } 
+  } 
+
+}
+
+# Input variable
+variable "filename" {
+  type    = string
+  default = "hello.txt"
+}
+
+# Local variable
+locals {
+  content = "Terraform cloud is good to learn"
+}
+
+# # Resource
+resource "local_file" "file1" {
+  filename = var.filename
+  content  = local.content
+}
+
+# # Output variable
+output "output_print" {
+  value = local_file.file1.id
+}
